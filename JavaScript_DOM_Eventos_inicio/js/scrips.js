388d4c7fc7ee71dc2df603bd000137ff191c57f7
CJGV-Work <Jovanni.villa35@gmail.com>
2022-06-16 08:05:45 -0500

    Update scrips.js

--- JavaScript_DOM_Eventos_inicio/js/scrips.js
+++ JavaScript_DOM_Eventos_inicio/js/scrips.js
@@ -26,4 +26,36 @@ enlaces[0].textContent='Sobre Nosotros'; //cambia el contenido del titulo
 const registro = document.getElementById('submit'); //seleccion de elementos por ID 
 
 console.log('registro'); // imprime el registro
-registro.value='super envio';//cambia el valor de el atributo value
+/* registro.value='super envio'; */ //cambia el valor de el atributo value
+
+/* generar enlace con JS */
+
+const nuevoEnlace= document.createElement('A');
+
+/* agregar el href */
+nuevoEnlace.href='http://www.google.com';
+/* agregar la clase */ 
+nuevoEnlace.classList.add('navegacion__enlace');
+/* agregar el Texto */
+nuevoEnlace.textContent='New Enlace-JS';
+console.log(nuevoEnlace);
+const navegacion= document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+/***** los eventos en Javascrip *****/
+
+window.addEventListener('load', function(){//load espera a que se cargue todo el archivo (html js Css img)
+    console.log(2);
+})
+
+document.addEventListener('DOMContentLoaded', function(){// solo espera a que se cargue HTML no Css js o img
+    console.log(4);
+})
+
+const botonEnviar= document.querySelector('.boton--primario');// selecciona dentro del html el que tiene la claseboton--primario
+botonEnviar.addEventListener('click', function(evento){//agrega un evento de escucha cuando se realice click, corre la funcion
+    console.log("enviando Informacion");
+    evento.preventDefault();// detinen o previene el comportamiento Default,
+    
+});
+
