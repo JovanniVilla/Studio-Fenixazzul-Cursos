feaac00b4db5a40a7f9be6fb0c821dd460f66a08
CJGV-Work <Jovanni.villa35@gmail.com>
2022-09-22 16:01:34 -0500

    first update
    primera practica de porgramacion

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,4 +1,7 @@
 
 console.log('Hola Mundo este es el programa mas facil del mundo');
+console.log('10');
+console.log(10);
+console.log('no se escribe los textons son comillas dentro de l parntese');     
 
 
